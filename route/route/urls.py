bc58677abcc7e4d021cffeac0938d840a5e6d03c
Yuta Sato <58802670+yutasato1112@users.noreply.github.com>
2025-03-30 12:28:58 +0900

    add preview page

--- route/route/urls.py
+++ route/route/urls.py
@@ -19,11 +19,12 @@ from django.urls import path
 from django.conf import settings 
 from django.conf.urls.static import static
 from cleaning.views import home as home_view
-
+from cleaning.views import preview as preview_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view.homeView.as_view(), name='home'),
+    path('preview/', preview_view.previewView.as_view(), name='preview'),
 ]
 
 if settings.DEBUG:
