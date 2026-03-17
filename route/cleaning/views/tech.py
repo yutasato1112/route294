9b94f25b2445b6513bef0aeee7893a7c06b78bc9
Yuta Sato <58802670+yutasato1112@users.noreply.github.com>
2026-03-17 10:10:48 +0900

    update technical page

--- route/cleaning/views/tech.py
+++ route/cleaning/views/tech.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
+from django.conf import settings
 
 class techView(TemplateView):
     template_name = "tech.html"
     def get(self, request, *args, **kwargs):
-        context = {}
-        return render(self.request, self.template_name, context)
-    
-    def post(self, request, *args, **kwargs):
-        context = {}
+        context = {
+            'app_version': getattr(settings, 'APP_VERSION', ''),
+        }
         return render(self.request, self.template_name, context)
