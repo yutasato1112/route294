14ef82971479d420a62a76774e88802adf294717
Yuta Sato <58802670+yutasato1112@users.noreply.github.com>
2026-03-19 00:30:13 +0900

    ver.1.5.4
    ログ修正

--- route/route/settings.py
+++ route/route/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Application version
-APP_VERSION = '1.5.3_mitsukaido'
+APP_VERSION = '1.5.4_mitsukaido'
 
 #email settings
 if os.path.exists(os.path.join('static/email.json')):
